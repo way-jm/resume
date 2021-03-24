adbf52ed122f11b3df4ec0f18377d61619f744b0
way <weizhengming@ahqianmo.com>
2021-03-24 09:39:22 +0800

    m

---
+++ build/precache-manifest.8a45f70be4cdde1a16e62740b60bb513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99daf81b35519d9e6b362c2ee14ae23d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67aa174a91631ce3ee84",
+    "url": "./static/css/main.a4ab2e12.chunk.css"
+  },
+  {
+    "revision": "365957e6fdfb13190f64",
+    "url": "./static/js/2.81d1ef4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.81d1ef4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67aa174a91631ce3ee84",
+    "url": "./static/js/main.56eb9863.chunk.js"
+  },
+  {
+    "revision": "22d8bd3b4e68820dd5a2",
+    "url": "./static/js/runtime-main.d53f019b.js"
+  }
+]);
